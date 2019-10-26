83e6dd96f76f06e4170eeb39c5351c02fa3eab0f
kammini1 <54959256+kammini1@users.noreply.github.com>
2019-10-26 17:25:23 -0400

    assignment 2 work

---
+++ textmining.py
@@ -0,0 +1,83 @@
+import urllib.request
+import random
+import string
+url='http://www.gutenberg.org/cache/epub/16328/pg16328.txt'
+response=urllib.request.urlopen(url)
+data=response.read()
+text=data.decode('utf-8')
+words=text.split()
+d=dict()
+for word in words:
+    strippables=string.punctuation+string.whitespace
+    word=word.strip(strippables)
+    word=word.lower()
+    word=word.replace('-','')
+    if word not in d:
+        d[word]=1
+    else:
+        d[word]+=1
+t=[]
+e=dict()
+excluding_stopwords=True
+stopwords_list=open('stopwords.txt')
+for line in stopwords_list:
+    stopwords=line.strip()
+    if stopwords not in e:
+        e[stopwords]=stopwords
+for word, frequency in d.items():
+    if word in e:
+        continue
+    else:
+        t.append((frequency,word))
+t.sort(reverse=True)
+number=int(input('Enter an integer.'))
+print('The',number, 'most frequent words in this text are:')
+for frequency,word in t[:number]:
+    print(word, '\t', frequency)
+
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+score=SentimentIntensityAnalyzer().polarity_scores(text)
+print(score)
+
+#hist={}
+#filename='Beowulf.txt'
+#fp=filename.decode('utf-8')
+#print(fp)
+
+#def skip_gutenberg_header(fp):
+        #for line in fp:
+            #if line.startswith('*** START OF THIS PROJECT'):
+                #break
+#skip_gutenberg_header(fp)
+
+#def process_file(filename,skip_header):
+    #if skip_header:
+        #skip_gutenberg_header(fp)
+    #for line in fp:
+        #if line.startswith('*** END OF THIS PROJECT'):
+            #break
+        #for word in line.split():
+        #if word not in hist:
+            #hist[word]=1
+        #else:
+            #hist[word]+=1
+#print(hist)
+
+#process_file(filename,skip_header=True)
+
+#def most_common(hist,excluding_stopwords=True):
+    #t=[]
+    #stopwords=process_file('stopwords.txt',False)
+    #stopwords=list(stopwords.keys())
+    #print(stopwords)
+    #for letter,freq in hist.items():
+        #if excluding_stopwords:
+            #if word in stopwords:
+                #continue 
+        #t.append((freq,word))
+
+#most_common(hist,excluding_stopwords=True)
+
+
